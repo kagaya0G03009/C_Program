6b56d4087ffa1f073356bb28e6a247289210c76f
kagaya <kagaya@kagaya-X555LAB>
2018-02-01 04:31:47 +0900

    add quick

---
+++ KobeDenshi_Group/src/Algorithms/Quick_Sort.h
@@ -0,0 +1,13 @@
+/*
+ * Quick_Sort.h
+ *
+ *  Created on: Feb 1, 2018
+ *      Author: kagaya
+ */
+
+#ifndef ALGORITHMS_QUICK_SORT_H_
+#define ALGORITHMS_QUICK_SORT_H_
+
+
+
+#endif /* ALGORITHMS_QUICK_SORT_H_ */
